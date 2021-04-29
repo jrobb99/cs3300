341d97543adaad73120afb58751f066c30291145
jrobb99 <jrobb@uccs.edu>
2021-04-29 03:43:51 +0000

    added edit test to rspec

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -16,4 +16,13 @@ RSpec.describe ProjectsController, type: :controller do
             expect(response).to be_success
         end
     end
+
+    context "GET #edit" do
+        let!(:project) { Project.create(title: "Test title", subtitle: "my subtitle", images: "path/to/image", description: 'test description') }
+        it "returns a success response" do
+            get :edit, params: { id: project }
+            expect(response).to be_success
+        end
+    end
+    
 end
